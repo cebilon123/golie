0dbe0079927c6121f7ddf2ac769980a4388069b1
Michał Kowal <michalkowal567@gmail.com>
2021-04-24 13:35:44 +0200

    Moved init go

--- app/init.go
+++ app/init.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"github.com/cebilon123/golie/app/cmd"
 	"github.com/spf13/cobra"
 )
 
@@ -11,8 +10,6 @@ func Init() {
 		Long: "Golie is distributed file share/sync CLI, which makes your memory lightweight and let you to store files" +
 			"safely on different devices as long as they are paired with each other.",
 	}
-
-	rootCmd.AddCommand(cmd.GetConfigureCommand())
 
 	if err := rootCmd.Execute(); err != nil {
 		println(err)
