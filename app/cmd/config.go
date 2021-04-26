763e94d65957672b548997721f80123bf2cc3c60
Michał Kowal <michalkowal567@gmail.com>
2021-04-26 21:29:17 +0200

    Add deserialization anf fix serialization

--- app/cmd/config.go
+++ app/cmd/config.go
@@ -1,7 +1,14 @@
 package cmd
 
+import cfg2 "github.com/cebilon123/golie/cfg"
+
 //SetPath sets path to directory which should be watched and synced
 func SetPath(path string) {
-	println(path)
+	if len(path) == 0 {
+		return
+	}
+
+	cfg := cfg2.Configuration{Path: path}
+	cfg.Serialize()
 }
 
--- cfg/cfg.go
+++ cfg/cfg.go
@@ -1,7 +1,10 @@
 package cfg
 
 import (
+	"bytes"
 	"encoding/binary"
+	"encoding/gob"
+	"fmt"
 	"os"
 )
 
@@ -21,21 +24,43 @@ func (c *Configuration) Equal(c2 *Configuration) bool {
 
 //OverwriteWithDifferentFields overwrites configuration with different values of c2. This is simplified (with use of ifs)
 //in sake of speed and efficiency.
-func (c *Configuration) OverwriteWithDifferentFields(c2 *Configuration) {
+func (c Configuration) OverwriteWithDifferentFields(c2 *Configuration) {
 	if (c.Path != c2.Path || len(c.Path) == 0) && len(c2.Path) > 0 {
 		c.Path = c2.Path
 	}
 }
 
+//Deserialize deserializes configuration file into Configuration struct.
 func Deserialize() *Configuration {
-	return nil
+	file, err := os.Open(configurationFileName)
+	if err != nil {
+		println(err)
+		_, err = os.Create(configurationFileName)
+		if err != nil {
+			println(err)
+		}
+	}
+
+	c := &Configuration{}
+	dec := gob.NewDecoder(file)
+	if err := dec.Decode(&c); err != nil {
+		fmt.Println(err)
+	}
+
+	err = binary.Read(file, binary.BigEndian, c)
+
+	if err != nil {
+		println(err)
+	}
+
+	return c
 }
 
 //Serialize serializes configuration into file. If there is existing config file it checks differences and overwrites only
 //different fields.
-func (c *Configuration) Serialize() {
+func (c Configuration) Serialize() {
 	existingConfig := Deserialize()
-	if existingConfig != nil && !c.Equal(existingConfig) {
+	if !c.Equal(existingConfig) {
 		c.OverwriteWithDifferentFields(existingConfig)
 	}
 
@@ -44,7 +69,13 @@ func (c *Configuration) Serialize() {
 		println(err)
 	}
 
-	err = binary.Write(f, binary.LittleEndian, c)
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(c); err != nil {
+		fmt.Println(err)
+	}
+
+	err = binary.Write(f, binary.BigEndian, buf.Bytes())
 	if err != nil {
 		println(err)
 	}
